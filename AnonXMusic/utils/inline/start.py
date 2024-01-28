a0d6fbc262a193cd7b6fe786bcc0d76dfc6d90d6
Shaurya (Levi) <139495856+Shauryanoobhai@users.noreply.github.com>
2024-01-29 00:24:27 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -31,7 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/Sung_Jin_Woo_Bot",
+            InlineKeyboardButton(text=_["S_B_7"], url=f"https://t.me/Sung_Jin_Woo_Bot",
         ],
     ]
     return buttons
